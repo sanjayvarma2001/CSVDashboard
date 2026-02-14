01b0fea076d1d0d4cc0b044a1e33ba301e64f749
Sanjay Varma <38099003+sanjayvarma2001@users.noreply.github.com>
2026-02-14 10:25:44 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -16,7 +16,8 @@ models.Base.metadata.create_all(bind= db.engine)
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins = ["http://localhost:3000"],
+                   allow_origins=["*"],
+                   allow_credentials=True,
                    allow_methods = ["*"],
                    allow_headers = ["*"],)
 
